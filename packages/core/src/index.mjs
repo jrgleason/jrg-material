2031d4db0c7fcac14991260e9219bf5ab3510daa
jrgleason <jackiegleason@gmail.com>
2020-05-04 13:24:24 -0400

    Trying to add the slider

--- packages/core/src/index.mjs
+++ packages/core/src/index.mjs
@@ -1,18 +1,19 @@
 import { Base } from './Base.mjs';
 import { ShadowElement } from './ShadowElement.mjs';
-// Add Mustache Globally
-if(typeof Mustache !== 'undefined'){
-  console.error("Mustache not found, templates not supported");
-  window.Mustache = {
-     render: (template) => template
-  }
-}
 function CREATE_ELEMENT(name, object, params) {
   customElements.get(name) ||
         customElements.define(name, object, params);
 }
 if(window){
+    // Add Mustache Globally
+    if(typeof Mustache === 'undefined'){
+      console.error("Mustache not found, templates not supported");
+      window.Mustache = {
+         render: (template) => template
+      }
+    }
     window.Base = Base;
     window.ShadowElement = ShadowElement;
     window.CREATE_ELEMENT = CREATE_ELEMENT;
 }
+export {CREATE_ELEMENT, ShadowElement, Base}
